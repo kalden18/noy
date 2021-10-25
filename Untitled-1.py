60ec91fa23508a16ee5618aace8dfa198e89ffd4
kalden18 <64891323+kalden18@users.noreply.github.com>
2021-10-25 13:22:49 +0545

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,19 +1,31 @@
 import pandas
 from sklearn import linear_model
+import numpy
 
-df = pandas.read_csv("C:/Users/kalden/documents/book.csv")
+df = pandas.read_csv("m 1.csv")
 
-X = df[['away', 'Home']]
-y = df['ftr']
+X = df[['hsot',"ashot","hshot",'asot','hs','as','hr','ar']]
+y = df['ag']
 
 regr = linear_model.LinearRegression()
 regr.fit(X, y)
 
-predictedCO2 = regr.predict([["5", "1"]])
-if predictedCO2 == 1:
-    print("home side wins !")
-elif predictedCO2 == 0:
-    print("away side wins !")
-elif predictedCO2 == 2:
-    print("its a draw !")    
+vX = df[['hsot',"ashot","hshot",'asot','hs','as','hr','ar']]
+vy = df['hg']
 
+vregr = linear_model.LinearRegression()
+vregr.fit(vX, vy)
+
+predictedC = regr.predict([[4,1,13,3,9,6,4.5,4.5]])
+cint = int(predictedC)
+predictedCO2 = round(cint)
+predictedCv = vregr.predict([[4,1,13,3,9,6,4.5,4]])
+cvint = int(predictedCv)
+predictedCO2v = round(cvint)
+train_x = predictedCO2[:80]
+train_y = predictedCO2v[:80]
+
+test_x = predictedCO2[80:]
+test_y = predictedCO2v[80:]
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+print(mymodel)
