3196dabb1cb39e3c675f254339e794be5711161e
kalden18 <64891323+kalden18@users.noreply.github.com>
2021-11-01 13:02:43 +0545

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-symbol=input("stock symbol: ")
+driver = webdriver.Edge("c:/users/kalden/documents/msedgedriver.exe")
+driver.get("https://www.youtube.com/user/peradze94")
+assert "Soccer Stories - Oh My Goal - YouTube" in driver.title
 
-URL="https://markets.businessinsider.com/stocks/"+symbol+"-stock"
-v=requests.get(URL)
-
-soup=BeautifulSoup(v.text,"html5lib")
-r=soup.find("span",{"class":"price-section__current-value"})
-v=str(r)
-b=v.replace('<span class="price-section__current-value">',"")
-f=str(b)
-c=f.replace("</span>","")
-rf=soup.find("span",{"class":"price-section__label"})
-vf=str(rf)
-bf=vf.replace('<span class="price-section__label">',"")
-ff=str(bf)
-cf=ff.replace("</span>","")
-print("current stock price is of "+str(cf)+"is : " + str(c))
+continue_link = driver.find_element(By.TAG_NAME,'span')
+elem = driver.find_element(By.CLASS_NAME,'style-scope ytd-video-meta-block').text
+channel= driver.find_element(By.TAG_NAME,'title').text
+v=str(elem)
+if "month" in v :
+    print("no new videos from"+str(channel))
+elif "hour" or"minutes" or"hours" or 'days' in v:
+    print("new videos from")  
+       
